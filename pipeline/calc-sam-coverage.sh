86c15af1c842665d20be31af206969013879f09d
C. Titus Brown <titus@idyll.org>
2014-09-17 10:15:28 -0400

    fix calc cov

--- pipeline/calc-sam-coverage.sh
+++ pipeline/calc-sam-coverage.sh
@@ -1,17 +1,17 @@
 for pos in 100 250 251 350
 do
-    X=$(samtools view mrna-reads.sorted.bam mrna14:${pos} | wc -l)
+    X=$(samtools view mrna-reads.sorted.bam mrna14:${pos}-${pos} | wc -l)
     echo mrna14 pos $pos count $X
 done
 
 for pos in 100 250 251 350
 do
-    X=$(samtools view mrna-reads.sorted.bam mrna134:${pos} | wc -l)
+    X=$(samtools view mrna-reads.sorted.bam mrna134:${pos}-${pos} | wc -l)
     echo mrna134 pos $pos count $X
 done
 
 for pos in 100 350 500 501 650
 do
-    X=$(samtools view mrna-reads.sorted.bam mrna124:${pos} | wc -l)
+    X=$(samtools view mrna-reads.sorted.bam mrna124:${pos}-${pos} | wc -l)
     echo mrna124 pos $pos count $X
 done
