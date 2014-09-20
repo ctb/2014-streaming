5731f71ff053b84d6239df8d1c1cef83c6a8751e
C. Titus Brown <titus@idyll.org>
2014-09-20 12:44:28 -0400

    updated clean; added sumarize pos file

---
+++ pipeline/summarize-pos-file.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+import sys
+import argparse
+import screed
+
+def read_pos_file(filename):
+    for line in open(filename):
+        line = line.strip()
+        try:
+            read, posns = line.split(' ', 2)
+            posns = map(int, posns.split(','))
+        except ValueError:
+            read = line
+            posns = []
+            
+        yield read, posns
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('posfile')
+    parser.add_argument('reads')
+    args = parser.parse_args()
+
+    posdict = dict(read_pos_file(args.posfile))
+    all_reads = set([ record.name for record in screed.open(args.reads) ])
+
+    n = 0
+    m = 0
+    for k, v in posdict.iteritems():
+        if not v:
+            continue
+
+        n += 1
+        m += len(v)
+
+    print 'posfile %s: %d mutated reads of %d; %d mutations total' % \
+          (args.posfile, n, len(all_reads), m)
+
+    
+if __name__ == '__main__':
+    main()
+
+
