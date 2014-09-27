71d662f9693903b4500ee343634d7b1ad54e34e8
C. Titus Brown <titus@idyll.org>
2014-09-27 07:35:26 -0600

    generate errhist from sam

---
+++ pipeline/sam-scan-errhist.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python
+import sys
+import argparse
+import screed
+
+
+MAX_SEQ_LEN = 5000
+
+
+def ignore_at(iter):
+    for item in iter:
+        if item.startswith('@'):
+            continue
+        yield item
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('genome')
+    parser.add_argument('samfile')
+    parser.add_argument('readfile')
+    args = parser.parse_args()
+
+    genome_dict = dict([ (record.name, record.sequence) for record in \
+                        screed.open(args.genome) ])
+
+    positions = [0] * MAX_SEQ_LEN
+    lengths = []
+
+    n = 0
+    for samline in ignore_at(open(args.samfile)):
+        n += 1
+        if n % 1000 == 0:
+            print >>sys.stderr, '...', n
+
+        readname, _, refname, refpos, _, _, _, _, _, seq = samline.split()[:10]
+        if refname == '*' or refpos == '*':
+            continue
+        
+        refpos = int(refpos)
+
+        #assert record.name == readname, (record.name, readname)
+        ref = genome_dict[refname][refpos-1:refpos+len(seq) - 1]
+        #print ref
+        #print seq
+
+        errors = []
+        for pos, (a, b) in enumerate(zip(ref, seq)):
+            if a != b:
+                positions[pos] += 1
+        lengths.append(len(seq))
+
+    # normalize for length
+    lengths.sort()
+    max_length = lengths[-1]
+
+    length_count = [0]*max_length
+    for j in range(max_length):
+        length_count[j] = sum([1 for i in lengths if i >= j])
+
+    # find the last point for which we have any data
+    last_zero = len(positions) - 1
+    while positions[last_zero] == 0:
+        last_zero -= 1
+
+    # write!
+    for n, i in enumerate(positions[:last_zero + 1]):
+        print n, i, float(i) / float(length_count[n])
+
+    print >>sys.stderr, "error rate: %.1f%%" % \
+          (100.0 * sum(positions) / float(sum(lengths)))
+
+if __name__ == '__main__':
+    main()
